f667c45ddbfa0dbceb465ab306c75be60b562271
Fernando Avellar Jr. <favellar@yahoo.com.br>
2023-12-13 14:25:26 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({ mensagem: "São acima da média!" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ mensagem: "API funcionando!" });
 }
 
 export default status;
